0e12205c67370043bcd6d9826fc5ea9db85ac202
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-05-01 20:47:42 -0600

    Constructed binary search tree

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,12 @@
 class BinarySearchTree {
-  // your code here
+  constructor(key = null, value = null, parent = null) {
+    this.key = key; 
+    this.value = value; 
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
 }
+
+const bst = new BinarySearchTree(5);
+console.log(bst); 
